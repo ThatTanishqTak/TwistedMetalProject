9570147161cd09706c2b630f9556a80035bb9ac5
ThatTanishqTak <tanishqtak3@gmail.com>
2025-02-04 19:34:29 +0000

    Final changes of the day

--- Assets/Scripts/Multiplayer/PlayerSpawner.cs
+++ Assets/Scripts/Multiplayer/PlayerSpawner.cs
@@ -7,6 +7,7 @@ public class PlayerSpawner : SimulationBehaviour, IPlayerJoined
 
     public void PlayerJoined(PlayerRef player)
     {
+        // Check to make sure we are spawning the right character
         if (Runner.LocalPlayer == player) { Runner.Spawn(PlayerPrefab, new Vector3(0.0f, 1.0f, 0.0f), Quaternion.identity); }
     }
 }
